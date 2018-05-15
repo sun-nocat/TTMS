6b30cdafc5933e368112a8ac9ee677881e6ebc86
sunmingming <1027908281@qq.com>
2018-05-15 22:19:33 +0800

    初始项目

---
+++ ttms/src/xupt/se/ttms/Test/StudioDAOTest.java
@@ -0,0 +1,33 @@
+package xupt.se.ttms.Test;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import xupt.se.ttms.dao.StudioDAO;
+import xupt.se.ttms.idao.iStudioDAO;
+
+public class StudioDAOTest {
+
+	iStudioDAO dao=new StudioDAO(); 
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelect() {
+		fail("Not yet implemented");
+	}
+
+}
